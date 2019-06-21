606a9cea8c3adc29556c3792acb47a58e6cb337c
hemstrow <whemstrom@ucdavis.edu>
2019-06-21 13:19:00 -0700

    removed old example data and updated stickSNPs to the snpRdata format

--- data-raw/stickSNPs.R
+++ data-raw/stickSNPs.R
@@ -1,16 +1,10 @@
-stickSNPs <- read.table("../../../../Stickleback/Full Data/2017_reruns/snps_numeric_filt.txt", stringsAsFactors = F, header = T, colClasses = "character")
-stickSNPs <- stickSNPs[grepl("group", stickSNPs$group),]
+x <- read.table("data/stick_NN_input.txt", stringsAsFactors = F, header = T, colClasses = "character")
 
-cols <- colnames(stickSNPs)
+stickSNPs <- import.snpR.data(x[,-c(1:3)],
+                              x[,1:3],
+                              data.frame(pop = substr(colnames(x)[-c(1:3)], 1, 3),
+                                         fam = rep(c("A", "B", "C", "D"), length = ncol(x) - 3),
+                                         stringsAsFactors = F),
+                              "NN")
 
-stickSNPs <- format_snps(stickSNPs, 3, 6, "0000", "00")
-colnames(stickSNPs) <- cols
-
-metadata <- stickSNPs[,1:3]
-stickSNPs <- stickSNPs[,4:ncol(stickSNPs)]
-
-stickSNPs <- cbind(metadata, stickSNPs[,order(colnames(stickSNPs))])
-
-stickSNPs <- stickSNPs[seq(1, nrow(stickSNPs), by = 10),]
-
-devtools::use_data(stickSNPs, overwrite = TRUE)
+usethis::use_data(stickSNPs, overwrite = TRUE)
